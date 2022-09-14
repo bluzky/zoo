61eda6fd230dd2e8df179359dc290af23569a685
Dung Nguyen <bluesky.1289@gmail.com>
2022-09-14 22:30:44 +0700

    Update release env

--- release.sh
+++ release.sh
@@ -11,5 +11,5 @@ mix compile --warnings-as-errors
 mix assets.deploy
 mix release
 
-_build/dev/rel/zoo/bin/zoo stop
-_build/dev/rel/zoo/bin/zoo daemon
+_build/prod/rel/zoo/bin/zoo stop
+_build/prod/rel/zoo/bin/zoo daemon
